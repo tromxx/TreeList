ade6519142c3b985fdc62e29061bf6c5b03cd9bd
DH <lovein6114@nate.com>
2023-08-25 01:12:53 +0900

    Member 엔티티 생성

--- src/main/java/com/TreeListProject/TreeList/reposiotry/MemberRepository.java
+++ src/main/java/com/TreeListProject/TreeList/reposiotry/MemberRepository.java
@@ -1,4 +1,10 @@
 package com.TreeListProject.TreeList.reposiotry;
 
-public interface MemberRepository {
+import com.TreeListProject.TreeList.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
