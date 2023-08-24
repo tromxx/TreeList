5e58e57dfa7dd327eff70fa8b733fa26a91138c5
husik927@gmail.com <husik927@gmail.com>
2023-08-24 14:02:19 +0900

    first commit

---
+++ src/test/java/com/TreeListProject/TreeList/TreeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TreeListProject.TreeList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
