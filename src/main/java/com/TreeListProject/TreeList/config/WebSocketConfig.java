03cc37e369762ea54b5f613ee8dffc5c4de42fb5
qhwkal1 <qhwkal1@naver.com>
2023-08-25 02:13:20 +0900

    socketConfig 수정

--- src/main/java/com/TreeListProject/TreeList/config/WebSocketConfig.java
+++ src/main/java/com/TreeListProject/TreeList/config/WebSocketConfig.java
@@ -1,11 +1,19 @@
 package com.TreeListProject.TreeList.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.logging.Logger;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
-public class WebSocketConfig {
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
 }
