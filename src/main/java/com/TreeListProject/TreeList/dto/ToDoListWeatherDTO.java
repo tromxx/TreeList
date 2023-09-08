eb745c07b06eaa11f53e667f269f3aeab88cd2c2
woojin <sun65568@hanmail.net>
2023-09-08 16:52:55 +0900

    09/08 날씨 가져오기(Entity)

--- src/main/java/com/TreeListProject/TreeList/dto/ToDoListWeatherDTO.java
+++ src/main/java/com/TreeListProject/TreeList/dto/ToDoListWeatherDTO.java
@@ -2,7 +2,7 @@ package com.TreeListProject.TreeList.dto;
 
 public class ToDoListWeatherDTO {
     private Long id; // primary key
-    private Long region; // 지역
+    private String region; // 지역
     private Double temp; // 온도
     private Double rainAmount; // 강수량
     private Double humid; // 습도
